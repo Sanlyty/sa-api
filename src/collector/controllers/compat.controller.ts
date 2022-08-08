4a7c8af69c70dbb04964cb61d244f4d36cf52438
Richard Ivanek <richard.ivanek@gmail.com>
2022-08-08 15:55:50 +0200

    Map and filter for compat endpoint

--- src/collector/controllers/compat.controller.ts
+++ src/collector/controllers/compat.controller.ts
@@ -9,7 +9,51 @@ import {
 import { LoggingInterceptor } from '../../logging.interceptor';
 import { MaintainerService } from '../services/maintainer.service';
 
-type QueryParams = Record<'from' | 'to', number | string | Date>;
+type QueryParams = Record<'from' | 'to', number | string | Date> & {
+    map?: string;
+    filter?: string;
+};
+
+const percRegex = /^perc-(\d+(?:\.\d+)?)$/;
+const getMapFromQuery = (
+    query: string,
+    data: { variants: string[]; data: [number, ...number[]][] }
+): ((data: number[]) => number) | undefined => {
+    if (query === 'sum' || query === 'avg') {
+        const factor = query === 'sum' ? 1 : data.variants.length;
+        return (row) => row.reduce((prev, next) => prev + next, 0) / factor;
+    }
+
+    const m = percRegex.exec(query);
+    if (m) {
+        const perc = Number.parseFloat(m[1]);
+        const n = Math.round(perc * (data.variants.length - 1));
+
+        return (row) => row.sort((a, b) => a - b)[n];
+    }
+
+    return undefined;
+};
+
+const filterRegex = /^(top|bot)-(\d+)$/;
+
+const getFilterFromQuery = (
+    query: string,
+    resp: { variants: string[]; data: [number, ...number[]][] }
+): number[] | undefined => {
+    const match = filterRegex.exec(query);
+
+    if (match) {
+        const factor = match[1] === 'top' ? -1 : 1;
+        const order = resp.variants
+            .map((_, i) => [i, resp.data.reduce((v, row) => v + row[i + 1], 0)])
+            .sort(([, a], [, b]) => (a - b) * factor);
+
+        return order.slice(0, Number.parseInt(match[2])).map(([i]) => i);
+    }
+
+    return undefined;
+};
 
 @Controller('api/v2/compat')
 @UseInterceptors(LoggingInterceptor)
@@ -32,12 +76,33 @@ export class CompatibilityController {
             (d) => new Date(Number.isNaN(Number(d)) ? d : Number(d))
         );
 
-        const data = await this.maintainerService.getMaintainerData(
+        let resp = await this.maintainerService.getMaintainerData(
             systemName,
             metricName,
             [from, to]
         );
 
-        return data;
+        const filter = getFilterFromQuery(qp.filter, resp);
+
+        if (filter) {
+            resp = {
+                variants: filter.map((i) => resp.variants[i]),
+                data: resp.data.map(([key, ...data]) => [
+                    key,
+                    ...filter.map((i) => data[i]),
+                ]),
+            };
+        }
+
+        const map = getMapFromQuery(qp.map, resp);
+
+        if (map) {
+            return {
+                variants: [qp.map],
+                data: resp.data.map(([key, ...values]) => [key, map(values)]),
+            };
+        }
+
+        return resp;
     }
 }
