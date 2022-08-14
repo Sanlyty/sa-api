233df04cbe26bd9d4f66e21e88f1936a5394fa69
Richard Ivánek <richard.ivanek@gmail.com>
2022-08-14 18:58:32 +0200

    Further improvements

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,12 +4,20 @@ import path = require('path');
 
 const { env } = process;
 
+dotenv.config();
+
+if (env.CONF_SA_API_PATH) {
+    path.join(env.CONF_SA_API_PATH, 'application.env');
+}
+
 const ensureExists = (key: string, warnOnly?: boolean) => {
     if (!env[key]?.length) {
         if (warnOnly) {
             console.warn(`Missing env var '${key}'`);
         } else {
-            console.error(`Missing env var '${key}'`);
+            console.error(
+                `Missing env var '${key}', this env var is mandatory.`
+            );
             process.exit(1);
         }
     }
@@ -18,12 +26,6 @@ const ensureExists = (key: string, warnOnly?: boolean) => {
 @Injectable()
 export class ConfigService {
     constructor() {
-        dotenv.config();
-
-        if (env.CONF_SA_API_PATH) {
-            path.join(env.CONF_SA_API_PATH, 'application.env');
-        }
-
         ensureExists('DATABASE_URL');
         ensureExists('CONF_MAINTAINER_MAP', true);
     }
@@ -55,15 +57,14 @@ export class ConfigService {
         if (!env.smtp_host) return undefined;
 
         const [host, port] = env.smtp_host.split(':');
-        const [user, pass] = (env.smtp_auth ?? '').split(':');
+        const [user, pass] = (env.smtp_auth ?? '')
+            .split(':')
+            .map((s) => s.trim() || undefined);
 
         return {
             host,
-            port: port ? Number(port) : undefined,
-            auth:
-                user || pass
-                    ? { user: user || undefined, pass: pass || undefined }
-                    : undefined,
+            port: port ? Number.parseInt(port) : undefined,
+            auth: user || pass ? { user, pass } : undefined,
         };
     }
 
