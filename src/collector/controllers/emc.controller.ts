9ebde851ca75458a0bfe973baf068b9826653e9e
Richard Ivánek <richard.ivanek@gmail.com>
2022-12-20 12:25:16 +0000

    EMC Capacity metrics

--- src/collector/controllers/emc.controller.ts
+++ src/collector/controllers/emc.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 
 import { LoggingInterceptor } from '../../logging.interceptor';
+import { PeriodType } from '../enums/period-type.enum';
 import { MaintainerService } from '../services/maintainer.service';
 
 const PerfMetrics: string[] = [
@@ -12,14 +13,37 @@ const PerfMetrics: string[] = [
     'CACHE_WP',
 ];
 
+const CapMetrics: string[] = [
+    'SUBSCRIBED_CAPACITY',
+    'SUBSCRIBED_CAPACITY_PERC',
+    'PHYSICAL_TOTAL',
+    'PHYSICAL_USED',
+    'PHYSICAL_FREE',
+    'PHYSICAL_USED_PERC',
+    'NET_TOTAL',
+    'NET_USED',
+    'NET_FREE',
+    'COMPRESSION_RATIO',
+    'PHYSICAL_USED_DAY',
+    'PHYSICAL_USED_WEEK',
+    'PHYSICAL_USED_MONTH',
+];
+
+const unitMap = {
+    percent: '%',
+    '1': '',
+};
+
 @Controller('api/v2/emc')
 @UseInterceptors(LoggingInterceptor)
 export class EmcController {
     constructor(private maintainers: MaintainerService) {}
 
     @Get('Performance')
-    public async getPerformanceMetrics() {
+    public async getPerformanceMetrics(@Query('period') period: PeriodType) {
         const result = [];
+
+        const suffix = period ? `_${period}` : '';
 
         for (const system of this.maintainers.getHandledSystems(['emc'])) {
             const entry = {
@@ -28,6 +52,41 @@ export class EmcController {
             };
 
             for (const metric of PerfMetrics) {
+                const info = await this.maintainers.getDatasetInfo(
+                    system,
+                    metric + suffix
+                );
+                const response = await this.maintainers.getLastMaintainerData(
+                    system,
+                    metric + suffix
+                );
+
+                entry.metrics.push({
+                    type: metric,
+                    date: response.date,
+                    unit: unitMap[info.units] ?? info.units,
+                    value: response.cols['average'] ?? 0,
+                    peak: (response.cols['peak'] ?? 0).toFixed(1),
+                });
+            }
+
+            result.push(entry);
+        }
+
+        return result;
+    }
+
+    @Get('Capacity')
+    public async getCapacityMetrics() {
+        const result = [];
+
+        for (const system of this.maintainers.getHandledSystems(['emc'])) {
+            const entry = {
+                name: system,
+                metrics: [],
+            };
+
+            for (const metric of CapMetrics) {
                 const info = await this.maintainers.getDatasetInfo(
                     system,
                     metric
@@ -39,9 +98,10 @@ export class EmcController {
 
                 entry.metrics.push({
                     type: metric,
-                    unit: info.units.replaceAll('percent', '%'),
+                    date: response.date,
+                    unit: unitMap[info.units] ?? info.units,
                     value: response.cols['average'] ?? 0,
-                    peak: (response.cols['peak'] ?? 0).toFixed(1),
+                    // peak: (response.cols['peak'] ?? 0).toFixed(1),
                 });
             }
 
